6157015a789d4d4986b02e3d6471535d82c9cbe3
Maeva Rodrigues <maeva.rodrigues@lapiscine.pro>
2024-03-07 15:37:43 +0100

    apresmidi

--- Cours/index.js
+++ Cours/index.js
@@ -6,7 +6,7 @@ var myNumber = 31
 
 var myName = "Maëva"
 
-console.log(myName)
+// console.log(myName)
 
 let myLastname = "Rodrigues"
 const myAge1 = 39
@@ -28,11 +28,11 @@ let firstNb = 23
 // result /= 8
 
 
-console.log(result)
+// console.log(result)
 
-result = 15
-result % 3
-const moduloresult = result % 3
+// result = 15
+// result % 3
+// const moduloresult = result % 3
 // console.log(moduloresult)
 
 // LES CHAINES DE CARACTERE 
@@ -62,17 +62,55 @@ let myBoolean = true
 let myBoolean2 = false
 
 if(myBoolean2) {
-    console.log(`ma condition est vraie`)
+    // console.log(`ma condition est vraie`)
 } else {
-    console.log(`ma condition est fausse`)
+    // console.log(`ma condition est fausse`)
 }
 
 let myCurrentAge = 39
 let legalAgeToDrive = 18
 
-if(myCurrentAge >= legalAgeToDrive) {
-    console.log('Tu as le droit de conduire')
-} else {
-    console.log(`Tu n'as pas le droit de conduire`)
+// if(myCurrentAge >= legalAgeToDrive) {
+    // console.log('Tu as le droit de conduire')
+// } else {
+    // console.log(`Tu n'as pas le droit de conduire`)
+
+
+// 
+let age1 = 12
+let age3 = 23 
+let age4 = 30
+let age5 = 70
+
+
+// 4 conditions chaînées pour afficher les résultats suivants : 
+// "Tu as moins de 10 ans"
+// "Tu as entre 11 et 30 ans
+// "Tu as netre 31 et 50 ans"
+//"Tu as plus de 50 ans"
+
+if(age1 <= 10)  {
+    //console.log("Tu as moins de 10 ans")
+} else if (age1 <= 30) {
+    //console.log("Tu as entre 11 et 30 ans")
+} else if (age1 <= 50) {
+    //console.log("Tu as netre 31 et 50 ans")
+} else  {
+    //console.log("Tu as netre 31 et 50 ans")
+} 
+
+//variables complexes : les objets
+
+const myIdentity = {
+    firstname: "Maëva",
+    lastname:"Rodrigues",
+    age: 39,
+    town: "Bordeaux"
 }
 
+console.log(myIdentity.firstname)
+
+const fullnameFromIdentity = myIdentity.firstname + ' ' + myIdentity.lastname
+console.log(fullnameFromIdentity)
+const myHello = `Bonjour je m'appelle` + ` `  + `${myIdentity.firstname}` ` `  ++  `j'ai` + ` `  + `${myIdentity.age}` + ` `  +`et j'habite` +  `${myIdentity.town}`
+console.log(myHello)
