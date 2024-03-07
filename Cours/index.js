a56844eabe0d6875b36ac725b45b3fd5a5cc8a9c
Maeva Rodrigues <maeva.rodrigues@lapiscine.pro>
2024-03-07 14:43:02 +0100

    apresdej

--- Cours/index.js
+++ Cours/index.js
@@ -18,7 +18,7 @@ myAge2 = 21
 
 // MANIP NBRE
 let firstNb = 23 
-let result = firstNb + 12 
+// let result = firstNb + 12 
 // result = result + 10
 // result += 10
 
@@ -33,19 +33,46 @@ console.log(result)
 result = 15
 result % 3
 const moduloresult = result % 3
-console.log(moduloresult)
+// console.log(moduloresult)
 
 // LES CHAINES DE CARACTERE 
 const firstName = "Mae"
-const lastName = 'Rodrigues'
-const sentence = "je m\'appelle Mae"
+const lastName = `Rodrigues`
+
+// const sentence = 'je m\'appelle Mae'
 
 //ES6 = 2015 JS MODERNE
-const fullName = "Je m'appelle"
+// const sentence = `je m'appelle Mae`
 
+// const fullName = ${firstName} + ---  ${lastName}
+// console.log(fullName)
 
+let name1 = "J'habite à "
+name1 += `Talence`
+// console.log(name1)
 
+let myTown = "Bordeaux"
+let myArea = 33
 
+const result = myTown + myArea
+// console.log(result2)
 
+// BOOLEENS
+let myBoolean = true
+let myBoolean2 = false
 
+if(myBoolean2) {
+    console.log(`ma condition est vraie`)
+} else {
+    console.log(`ma condition est fausse`)
+}
+
+let myCurrentAge = 39
+let legalAgeToDrive = 18
+
+if(myCurrentAge >= legalAgeToDrive) {
+    console.log('Tu as le droit de conduire')
+} else {
+    console.log(`Tu n'as pas le droit de conduire`)
+}
 
