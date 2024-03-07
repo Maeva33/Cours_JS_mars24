3d29e320ee18ab281592e4ded9fc5bef494edf49
Maeva Rodrigues <maeva.rodrigues@lapiscine.pro>
2024-03-07 14:29:21 +0100

    apres dej

---
+++ Cours/index.js
@@ -0,0 +1,51 @@
+// alert('Salut tout le monde')
+
+// console.log("Hello console")
+
+var myNumber = 31 
+
+var myName = "Maëva"
+
+console.log(myName)
+
+let myLastname = "Rodrigues"
+const myAge1 = 39
+let myAge2 = 40 
+
+myAge2 = 21
+
+// console.log(myAge2)
+
+// MANIP NBRE
+let firstNb = 23 
+let result = firstNb + 12 
+// result = result + 10
+// result += 10
+
+// result -= 5
+
+// result *= 2
+// result /= 8
+
+
+console.log(result)
+
+result = 15
+result % 3
+const moduloresult = result % 3
+console.log(moduloresult)
+
+// LES CHAINES DE CARACTERE 
+const firstName = "Mae"
+const lastName = 'Rodrigues'
+const sentence = "je m\'appelle Mae"
+
+//ES6 = 2015 JS MODERNE
+const fullName = "Je m'appelle"
+
+
+
+
+
+
+
