132e9e65cc90ab060185a236bbc727c424d2ee91
Maeva Rodrigues <maeva.rodrigues@lapiscine.pro>
2024-03-15 16:42:26 +0100

    Add filter method explanations

--- Cours/Exercices/scripts/index_a.js
+++ Cours/Exercices/scripts/index_a.js
@@ -1,29 +1,7 @@
-
-//Afficher dans la console une chaîne de caractere de tous les ingrédients de la dernière pizza : "mozzarella, tomate, jambon, ananas" avec une boucle for:
-
-//?console.log(pizzas[pizzas.length - 1].ingredients + "") HACK ; )
-
-// let result = "";  //déclare une variable result et lui attribue une valeur initiale de chaîne vide. Cette variable sera utilisée pour stocker la chaîne résultante des ingrédients de la pizza.
-// let ingredients = pizzas[pizzas.length - 1].ingredients;   
-//Déclare une variable ingredients et lui attribue le tableau d'ingrédients de la dernière pizza du tableau pizzas.
-
-// for (let i = 0; i < ingredients.length; i++) {   
-//     // Démarre une boucle for qui itérera sur chaque élément du tableau ingredients. La variable i est initialisée à 0, la boucle continue tant que i est inférieur à la longueur du tableau ingredients, et à chaque itération, i est incrémentée de 1.
-//     if (i === ingredients.length - 1) {
-//         // vérifie si l'indice actuel i est égal à la longueur du tableau ingredients moins 1, ce qui signifie que c'est le dernier ingrédient dans le tableau.
-//         result += ingredients[i];
-//         //  Si c'est le cas, ajoute l'ingrédient à la variable result.
-//     } else {
-//         result += ingredients[i] + ", ";
-//         // Si ce n'est pas le dernier ingrédient, ajoute l'ingrédient suivi d'une virgule et d'un espace à la variable result.
-//     }
-// }
-// console.log(result);
-// Affiche la chaîne result dans la console une fois que la boucle est terminée.
-
-//! CORRECTION DE PAUL :
 import pizzas from "../data/pizzas.json" assert {type: "json"}
+import pizzas_2 from "../data/pizzas_2.json" assert {type: "json"}
 
+// Afficher dans la console une chaîne de caractère de tous les ingrédients de la dernière pizza : "mozzarella, tomate..."
 let result = ""
 const ingredientsArray = pizzas[pizzas.length - 1].ingredients
 
@@ -37,54 +15,9 @@ for (let i = 0; i < ingredientsArray.length; i++) {
 
 // console.log(result)
 
-// Afficher le nom de la pizza la plus chère :
-
-let maxPrice = [0] 
-let pizzaName = ""
-for (let i = 0; i < pizzas.length; i++) {
-    if (pizzas[i].price > maxPrice) {
-        maxPrice = pizzas[i].price
-        pizzaName = pizzas[i].name
-    }
-}
-//console.log(pizzaName)
-
-//TODO Initialisation des variables :
-
-// TODO Nous initialisons deux variables : maxPrice qui stockera le prix maximum trouvé et pizzaName qui stockera le nom de la pizza correspondant à ce prix maximum. maxPrice est initialisé à [0] et pizzaName est initialisé à une chaîne vide "".
-// TODO Boucle de parcours des pizzas :
-
-//TODO  Une boucle for pour parcourir toutes les pizzas dans le tableau pizzas.
-// TODO La condition i < pizzas.length garantit que  chaque pizza est parcourue dans le tableau.
-
-// TODO Vérification du prix maximum :
-// TODO Dans chaque itération de la boucle, le prix de la pizza actuelle est comparée (pizzas[i].price) avec le prix maximum trouvé jusqu'à présent (maxPrice).
-
-//TODO  Si le prix de la pizza actuelle est supérieur au prix maximum actuel, cela signifie que nous avons trouvé une nouvelle pizza avec un prix plus élevé.
-//TODO  Nous mettons donc à jour maxPrice avec le prix de la pizza actuelle et pizzaName avec le nom de cette pizza.
-
-//TODO  Affichage du nom de la pizza la plus chère :
-// TODO Une fois que la boucle a parcouru toutes les pizzas et a trouvé la pizza la plus chère, nous affichons le nom de cette pizza dans la console en utilisant console.log(pizzaName).
-
-// TODO En résumé, ce code parcourt toutes les pizzas pour trouver celle avec le prix maximum, puis affiche le nom de cette pizza.
-
-// ! CORRECTION DE PAUL :
-
-let maxPizza = pizzas[0]
-
-
-function getMostExpensivePizza() {
-}
-for (let i = 1; i < pizzas.length; i++) {
-    if (pizzas[i].price > maxPizza.price) {
-        maxPizza = pizzas[i]
-    }
-}
-console.log(`La ${maxPizza.name} a pour ingrédients ${maxPizza.ingredients} et coûte ${maxPizza.price}`)
-
-// En paramètre, un tableau de pizzas, on retourne la pizza la plus chère
+// Afficher le nom et les ingrédients de la pizza la plus chère
 const mostExpPizza = getMostExpensivePizza(pizzas)
-console.log(mostExpPizza.name)
+// console.log(mostExpPizza.name)
 
 // En paramètre, un tableau de pizzas, on retourne la pizza la plus chère
 function getMostExpensivePizza(arrPizzas) {
@@ -99,11 +32,7 @@ function getMostExpensivePizza(arrPizzas) {
     return maxPizza
 }
 
-
 // console.log(`La ${maxPizza.name} a pour ingrédients ${maxPizza.ingredients} et coûte ${maxPizza.price}`)
-
-import pizzas from "../data/pizzas.json" assert {type: "json"}
-import pizzas_2 from "../data/pizzas_2.json" assert {type: "json"}
 
 // Ecrire une fonction qui cherche la pizza la plus chère parmi 2 tableaux passés en paramètre
 // Array.push() => la méthode qui permet d'ajouter un élément à la fin d'un tableau
@@ -122,6 +51,113 @@ function getUltimatePizza(arr1, arr2) {
     return getMostExpensivePizza(totalArray)
 }
 
+
+// Ecrire une fonction qui prend en paramète un tableau de pizzas, et qui me retourne le prix moyen de toutes les pizzas
+const resultAvg = getAveragePrice(pizzas)
+// console.log(resultAvg)
+
+function getAveragePrice(arr) {
+    let total = 0
+
+    for (let i = 0; i < arr.length; i++) {
+        total += arr[i].price
+    }
+
+    const average = total / arr.length
+
+    // Méthode alternative pour tronquer un nombre après la virgule
+    return average.toFixed(2)
+}
+// Ecrire une fonction qui prend 2 paramètres : un tableau de pizza et un nom de pizza. Cette fonction retourne un tableau des ingrédients de cette pizza
+const resultIngredients = getIngredientsByName(pizzas, "Regina")
+// console.log(resultIngredients)
+
+function getIngredientsByName(arr, nameParam) {
+    let ingredientsArr
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].name === nameParam) {
+            ingredientsArr = arr[i].ingredients
+            // Une fois l'element trouve, il est inutile de continuer a executer la boucle
+            break
+        }
+    }
+    return ingredientsArr
+}
+
+// Ecrire une fonction qui prend en parametre un tableau de pizzas, et qui retourne un tableau des noms de pizzas qui sont a base de tomate
+const resultNames = getPizzasName(pizzas)
+// console.log(resultNames)
+
+
+
+// Amélioration, la fonction prend en 2ème paramètre un tableau de noms de pizzas, et retourne un tableau des ingrédients COMMUNS à ces pizzas
+
+
+
+function getPizzasNameWithTomato(arr) {
+    let pizzaNames = []
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].baseTomate) pizzaNames.push(arr[i].name)
+    }
+    return pizzaNames
+}
+
+function getPizzasName(arr) {
+    let pizzaNames = []
+
+    for (let i = 0; i < arr.length; i++) {
+        pizzaNames.push(arr[i].name)
+    }
+
+    return pizzaNames
+}
+
+// NOUVELLE SYNTAXE DES FONCTIONS : LES FONCTIONS FLECHEES, ARROW FUNCTIONS
+const container = document.querySelector('.container')
+
+// fonction anonyme
+const myFunction = (e) => {
+    console.log(e.currentTarget)
+}
+
+container.addEventListener('click', () => { })
+
+const names = pizzas.map((element) => {
+    let result
+    if (element.baseTomate) {
+        result = "Pizza a la tomate : " + element.name
+    } else {
+        result = "Pizza a la creme fraiche : " + element.name
+    }
+
+    return result
+})
+
+// console.log(pizzas.map(el => el.name))
+console.log(pizzas.map(el => el.baseTomate ? "Pizza a la tomate : " + el.name : "Pizza a la creme fraiche : " + el.name))
+// console.log(names)
+
+const numbers = [2, 5, 34, 32, 12]
+
+// a l'aide d'un .map, afficher un tableau qui contient tous les doubles de numbers
+const doubles = numbers.map((number) => {
+    return number * 2
+})
+
+// les elements qui ne remplissent pas la condition donne quand meme un undefined
+const smallNumbers = numbers.map((number) => {
+    if (number < 10) return number
+})
+
+// const filteredNumbers = numbers.filter((element) => {
+//     return element < 10
+// })
+
+const filteredNumbers = numbers.filter(element => element < 10)
+console.log(filteredNumbers)
+
+// a l'aide d'un filter, refaire l'exercice pour filtrer les pizzas avec tomate
 
 
 
