02678463bcef0e3a2f8dd5da04834e89877d4c57
Maeva Rodrigues <maeva.rodrigues@lapiscine.pro>
2024-03-18 14:21:53 +0100

    commit after meal

--- Cours/Exercices/scripts/index_b.js
+++ Cours/Exercices/scripts/index_b.js
@@ -29,14 +29,38 @@ function getBestMovie(arr) {
     return best
 }
 
-
-
-
-
-
 // e. Logger le titre du film le plus ancien
+function getolderMovie(arr)
+{
+    let olderMovie = arr[0];
+    for(let i; i < arr.length; i ++ )
+    {
+        if(arr[i].year < olderMovie.year)
+        {
+            olderMovie = arr[i];
+        }
+    }
+    return olderMovie.year;
+}
+
+console.log(getolderMovie(movies))
+
 // f. Logger un tableau de tous les titres de film qui ont au moins 3 acteurs
+function getMoviesWith3Actors(arr) {
+    let moviesWith3Actors = []
+    arr.forEach((element) => {
+        if (element.actors.length >= 3) {
+            moviesWith3Actors.push(element.title)
+        }
+    })
+    return moviesWith3Actors
+}
+console.log(getMoviesWith3Actors(movies))
 
 // 1. Ecrire une fonction qui prend en parametre le tableau movies et qui retourne un nouveau tableau de tous les titres de films
+
+
+
+
 // 2. Ecrire une fonction qui prend en parametre une annee et un tableau (movies), et qui retourne un nouveau tableau de titres de film de l'annee specifiee
 // 3. Ecrire une fonction qui prend en parametre un realisateur et un tableau (movies), et qui retourne un nouveau tableau de tous les titres de films de ce realisateur
