d00688bf89bc39e0ae3a3f5af3f210c4b00bcd95
Maeva Rodrigues <maeva.rodrigues@lapiscine.pro>
2024-03-19 15:12:47 +0100

    end pool

--- Cours/Exercices/bordeaux_opendata/index.js
+++ Cours/Exercices/bordeaux_opendata/index.js
@@ -14,10 +14,10 @@ function onBtnClick() {
 
 function btnChange(btnState) {
     if (btnState === "search") {
-        button.textContent = "..."
+        button.innerHTML = `<div class="lds-ellipsis"><div></div><div></div><div></div><div>`
         button.classList.add('searching')
     } else {
-        button.textContent = "Actualiser"
+        button.innerHTML = "Actualiser"
         button.classList.remove('searching')
     }
 
@@ -39,21 +39,39 @@ async function fetchData() {
 function displayData(pools) {
     // on rafraîchit le poolsContainer
     poolsContainer.innerHTML = ""
+
+    // Implémenter le tri du tableau par odre décroissant d'entrées
+    // pools.sort((a, b) => {
+    //     return b.entree - a.entree
+    // })
+
+    // Implémenter le tri du tableau par odre décroissant de ratio d'occupation
+    pools.sort((a, b) => {
+        return b.entree / b.fmizonmax - a.entree / a.fmizonmax
+    })
+
     pools.forEach(pool => {
-        console.log(pool)
         const div = document.createElement('div')
         //<div class="place" data-index="0">
         div.classList.add("place")
         div.innerHTML = `
-        <div class="place-name">nom de la piscine</div>
-        <div class="place-infos">... / ...</div>
-        <div class="diagram-container">
-          <div class="capacity"></div>
-          <div class="visitors"></div>
-        </div>
+            <div class="place-name">${pool.etablissement_etalib} (${pool.fmizonlib})</div>
+            <div class="place-infos">${pool.entree} / ${pool.fmizonmax}</div>
+            <div class="diagram-container">
+                <div class="capacity"></div>
+                <div class="visitors"></div>
+            </div>
         `
         poolsContainer.appendChild(div)
     });
 
-    console.log(pools)
+    const visitorDiagrams = document.querySelectorAll('.visitors')
+
+    setTimeout(() => {
+        visitorDiagrams.forEach((element, index) => {
+            const ratio = pools[index].entree / pools[index].fmizonmax
+            element.style.transform = `scaleX(${ratio})`
+        })
+    }, 10)
+
 }
